3a03353b2c5528aead793fbb5e2bd330eacc0b4f
Tom Randolph <thomas.randolph26@gmail.com>
2026-06-26 16:59:55 +0100

    start to add pg

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,24 @@
 use axum::{Router, routing::get};
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@localhost/test")
+        .await
+        .unwrap();
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+    assert_eq!(row.0, 150);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
